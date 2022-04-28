b4f74ec6004c8e5c96e2a2932a99eef304d58ae4
DogacOzates <67386086+DogacOzates@users.noreply.github.com>
2022-04-27 23:53:25 -0700

    Add files via upload

---
+++ m0921-redux-main/src/components/Drink.js
@@ -0,0 +1,27 @@
+import React, { memo } from "react";
+
+const Drink = memo(({ drink, handleQuantity }) => {
+  return (
+    <div className="card">
+      <div className="card-body fw-bold">
+        {drink.beverage} : {drink.quantity}
+      </div>
+      <div className="card-footer">
+        <button
+          className="btn btn-primary m-1"
+          onClick={() => handleQuantity("+", drink.id)}
+        >
+          +
+        </button>
+        <button
+          className="btn btn-danger m-1"
+          onClick={() => handleQuantity("-", drink.id)}
+        >
+          -
+        </button>
+      </div>
+    </div>
+  );
+});
+
+export default Drink;
